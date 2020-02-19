1079ad3b0c3576db7497c7d388451c37a7794319
Jenna Goldman <jenna.goldman89@gmail.com>
2020-02-19 11:47:24 -0800

    add weather endpoint

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const data = require('./geo.js');
+const weather = require('./weather.js');
 const app = express();
 // const request = require('superagent');
 
-console.log(data);
+let lat;
+let lng;
 
 app.get('/', (request, respond) => respond.send('Jello World!'));
 
 app.get('/location', (request, respond) => {
     const cityData = data.results[0];
 
+    lat = cityData.geometry.location.lat;
+    lng = cityData.geometry.location.lng;
+
     respond.json({
         formattedQuery: cityData.formatted_address,
         latitude: cityData.geometry.location.lat,
         longitude: cityData.geometry.location.lng
     });
+});
+
+const getWeatherData = (lat, lng) => {
+    return weather.daily.data.map(forecast => {
+        return {
+            forecast: forecast.summary,
+            time: new Date(forecast.time * 1000)
+        };
+    });
+};
+
+app.get('/weather', (require, respond) => {
+    const portlandWeather = getWeatherData(lat, lng);
+
+    respond.json({ portlandWeather });
 });
 
 module.exports = { app };
