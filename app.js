f12a6037ded5ab642f4a0595c52a1961bdeeda21
Jenna Goldman <jenna.goldman89@gmail.com>
2020-02-19 16:36:51 -0800

    add trails endpoint

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const weather = require('./weather.js');
 const request = require('superagent');
 const app = express();
 
@@ -15,7 +14,6 @@ app.get('/location', async(req, res, next) => {
         const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;
         const data = await request.get(URL);
         const firstLocation = data.body[0];
-        
 
         latitude = firstLocation.lat;
         longitude = firstLocation.lon;
@@ -30,8 +28,11 @@ app.get('/location', async(req, res, next) => {
     }
 });
 
-const getWeatherData = (latitude, longitude) => {
-    return weather.daily.data.map(forecast => {
+const getWeatherData = async(latitude, longitude) => {
+    const URL = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${latitude},${longitude}`;
+    const data = await request.get(URL);
+
+    return data.body.daily.data.map(forecast => {
         return {
             forecast: forecast.summary,
             time: new Date(forecast.time * 1000)
@@ -39,10 +40,43 @@ const getWeatherData = (latitude, longitude) => {
     });
 };
 
-app.get('/weather', (require, respond) => {
-    const portlandWeather = getWeatherData(latitude, longitude);
+app.get('/weather', async(req, res, next) => {
+    try {
+        const locationWeather = await getWeatherData(latitude, longitude);
 
-    respond.json({ portlandWeather });
+        res.json({ locationWeather });
+    } catch (err) {
+        next(err);
+    }
+});
+
+const getTrailsData = async(latitude, longitude) => {
+    const URL = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxResults=10&key=${process.env.TRAILS_API_KEY}`;
+    const data = await request.get(URL);
+    console.log(data);
+    return data.body.trails.map(trail => {
+        return {
+            name: trail.name,
+            location: trail.location,
+            length: trail.length,
+            stars: trail.stars,
+            star_votes: trail.starVotes,
+            summary: trail.summary,
+            trail_url: trail.url,
+            conditions: trail.conditionStatus,
+            condition_date: trail.conditionDate
+        };
+    });
+};
+
+app.get('/trails', async(req, res, next) => {
+    try {
+        const locationTrails = await getTrailsData(latitude, longitude);
+
+        res.json({ locationTrails });
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = { app };
